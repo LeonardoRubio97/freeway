46664c415aa25e08a6aab52890f9155e8acd73f1
Leonardo Rubio <leonardo.fmedeiros@hotmail.com>
2022-08-22 15:57:32 -0300

    first commit

---
+++ README.txt
@@ -0,0 +1,16 @@
+# Esse jogo foi baseado no clássico jogo da Atari, Freeway #
+
+Para jogar basta clicar no botão play e em seguida clicar na janela do jogo.
+
+O objetivo é simples, basta levar a vaquinha até o outro lado da estrada. Se conseguir, o contador de pontos soma 1 ponto e, caso seja atingido por um carro você perde 1 ponto.
+
+Para jogar é só usar as setas do teclado CIMA e BAIXO.
+Obs: vaquinha não anda para os lados (ainda).
+
+
+
+O desenvolvimento
+
+O jogo foi desenvolvido com base no curso da Alura, onde é obordado temas como, arrays, clean code, laços de repetição e lógica de programação. 
+Durante as aulas foi possível colocar em prática o uso das functions, elementos dos arrays que representam as posições nos eixos x e y de cada personagem e a estrutura de repetição usando if.
+
---
+++ js/carro.js
@@ -0,0 +1,39 @@
+// código do carro
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [40, 96, 150, 210, 270, 318];
+let velocidadeCarros = [2, 2.5, 3.2, 5, 3.3, 4];
+let comprimentoCarro = 50;
+let alturaCarro = 40;
+
+
+
+//exibe carro na tela
+function mostraCarro(){
+  for(let i = 0; i < imagemCarros.length; i++){
+  image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+    
+  }
+}
+  
+
+//movimenta carro
+function movimentaCarro(){
+  for(let i = 0; i < imagemCarros.length; i++){
+  xCarros[i] -= velocidadeCarros[i];  
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+  for(let i = 0; i < imagemCarros.length; i++){
+  if (passoTodaATela(xCarros[i])){
+    xCarros[i] = 600;
+    
+  }
+ }
+}
+
+function passoTodaATela(xCarro){
+  return xCarro < - 50;
+  
+}
+
